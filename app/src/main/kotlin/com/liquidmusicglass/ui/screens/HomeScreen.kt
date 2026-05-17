8e73a4b01be363a9f8220576fdd0626f90d31bb9
root <root@localhost.localdomain>
2026-05-17 02:37:49 +0000

    fix: add width import to HomeScreen.kt

--- app/src/main/kotlin/com/liquidmusicglass/ui/screens/HomeScreen.kt
+++ app/src/main/kotlin/com/liquidmusicglass/ui/screens/HomeScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
