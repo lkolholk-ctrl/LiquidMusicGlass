abb554732d41b6aad1370732b2160f193b42a83f
VPS User <lkolholk@gmail.com>
2026-05-18 09:18:29 +0000

    Fix build errors: remove duplicate @Serializable, add missing size import

--- app/src/main/kotlin/com/liquidmusicglass/ui/player/LyricsSheet.kt
+++ app/src/main/kotlin/com/liquidmusicglass/ui/player/LyricsSheet.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
